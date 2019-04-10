177734c575cff57185f59bca7391f8472f689fdd
MohaElder169 <calen0909@hotmail.com>
2019-04-10 18:48:06 +0800

    Update app.js

--- app.js
+++ app.js
@@ -387,36 +387,6 @@ App({
     ],
     userInfo: null
   },
-  show: function (that, param, opacity) {
-    var animation = wx.createAnimation({
-      //持续时间800ms
-      duration: 800,
-      timingFunction: 'ease',
-    });
-    //var animation = this.animation
-    animation.opacity(opacity).step()
-    //将param转换为key
-    var json = '{"' + param + '":""}'
-    json = JSON.parse(json);
-    json[param] = animation.export()
-    //设置动画
-    that.setData(json)
-  },
-
-  //滑动渐入渐出
-  slideupshow: function (that, param, px, opacity) {
-    var animation = wx.createAnimation({
-      duration: 800,
-      timingFunction: 'ease',
-    });
-    animation.translateY(px).opacity(opacity).step()
-    //将param转换为key
-    var json = '{"' + param + '":""}'
-    json = JSON.parse(json);
-    json[param] = animation.export()
-    //设置动画
-    that.setData(json)
-  },
 
   //向右滑动渐入渐出
   sliderightshow: function (that, param, px, opacity) {
